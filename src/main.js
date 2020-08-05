49b4ea468a3e5c6ede876dcbf4ca43b1a9be9e81
wassfila <wassim.filali@gmail.com>
2020-08-05 19:59:03 +0200

    multiple tiles

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import {html,clear,send, defined, add_style_element} from "../libs/web-js-utils.js"
 import {fetch_json} from "./utils.js"
-import {Tile} from "./tile.js"
+import {TileGroup} from "./tile.js"
 import {Mqtt} from "./mqtt_app.js"
 
-let tile
+let tg
 
 function onMqttMessage(e){
     const topic = e.detail.topic;
     const payload = e.detail.payload
     if(topic == "lifx/sim_tiles"){
-        tile.update(payload.colors)
+        //tile.update(payload.colors)
     }
 }
 
@@ -24,16 +24,17 @@ function svg_to_image(svg){
       
        let canvas = document.createElement('canvas');
        
-       canvas.widht = 300;
+       canvas.widht = svg.width.baseVal.value;
        
-       canvas.height = 300;
+       canvas.height = svg.height.baseVal.value;
        let context = canvas.getContext('2d');
        // draw image in canvas starting left-0 , top - 0  
-       context.drawImage(image, 0, 0, 300, 300 );
+       context.drawImage(image, 0, 0, svg.width.baseVal.value, svg.height.baseVal.value );
       //  downloadImage(canvas); need to implement
       let cls = context.getImageData(150,190,1,1).data
       console.log(cls)
-      tile.update([`rgb(${cls[0]},${cls[1]},${cls[2]})`])
+      //tile.update([`rgb(${cls[0]},${cls[1]},${cls[2]})`])
+      tg.update_canvas(context)
     };
     image.src = blobURL;
 }
@@ -42,9 +43,11 @@ async function main(){
     console.log("main() start")
 
     let parent = document.body
-    let [width,height] = [300,300]
+    let [width,height] = [parent.offsetWidth,300]
     let svg = html(parent,/*html*/`<svg id="main_svg" xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}"></svg>`);
-
+    
+    html(svg,/*html*/`<rect x="0" y="0" width="${width}" height="${height}" stroke="black" stroke-width="1" fill="green" fill-opacity="1" />`);
+    
     html(svg,
         /*html*/`<defs>
             <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
@@ -56,19 +59,23 @@ async function main(){
         );
 
 
-    svg_to_image(svg)
+    let sim_svg = html(parent,/*html*/`<svg id="sim_svg" xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}"></svg>`);
+    html(sim_svg,/*html*/`<rect x="0" y="0" width="${width}" height="${height}" stroke="black" stroke-width="1" fill="grey" fill-opacity="1" />`);
 
-    let sim_svg = html(parent,/*html*/`<svg id="main_svg" xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}"></svg>`);
 
-
-    tile = new Tile(sim_svg,0,0)
+    tg = new TileGroup(sim_svg)
+    tg.init("host_1")
+    //tile = new Tile(sim_svg)
+    //await tile.init(0,0)
     //tile.update(["blue","red","green"])
-    let colors = tile.extract()
+    //let colors = tile.extract()
 
     window.addEventListener( 'mqtt_message', onMqttMessage, false);
 
     let mqtt = new Mqtt()
     await mqtt.connect()
+
+    svg_to_image(svg)
 
 }
 
--- src/tile.js
+++ src/tile.js
@@ -1,27 +1,33 @@
 import {Svg} from "../libs/svg_utils.js"
 import {html,clear,send, defined, add_style_element} from "../libs/web-js-utils.js"
+import {fetch_json} from "./utils.js"
 
 let utl = new Svg()
-
+let config
 class Tile{
-    constructor(parent_svg,x,y){
+    constructor(parent_svg){
         this.svg = parent_svg
+    }
+    init(x,y){
+        this.led_w = config.graphics.led.width_pixels
+        this.led_h = config.graphics.led.height_pixels
         this.x = x
         this.y = y
+        this.leds = []
         for(let j=0;j<8;j++){
             for(let i=0;i<8;i++){
-                let x = this.x + i*20
-                let y = this.y + j*20
-                let w = 20
-                let h = 20
-                html(this.svg,
-                /*html*/`<rect x="${x}" y="${y}" rx="3" width="${w}" height="${h}" stroke="black" stroke-width="1" fill-opacity="0"/>`
-                );
+                let x = this.x + i*this.led_w
+                let y = this.y + j*this.led_h
+                let rect = html(this.svg,
+                /*html*/`<rect id="led_${x}_${y}" x="${x}" y="${y}" rx="4" width="${this.led_w}" height="${this.led_h}" stroke="black" fill="grey" stroke-width="1"/>`);
+                this.leds.push(rect)
             }
         }
-    }
+        html(this.svg,
+            /*html*/`<rect x="${x}" y="${y}" rx="4" width="${this.led_w*8}" height="${this.led_h*8}" stroke="black" fill-opacity="0" stroke-width="4"/>`);
+}
 
-    update(colors){
+    update_colors(colors){
         let index = 0
         let length = colors.length
         for(let j=0;j<8;j++){
@@ -41,12 +47,48 @@ class Tile{
             }
         }
     }
+
+    update_canvas(context){
+        let index = 0
+        for(let j=0;j<8;j++){
+            for(let i=0;i<8;i++){
+                let x = this.x + (i+0.5)*this.led_w
+                let y = this.y + (j+0.5)*this.led_h
+                let cls = context.getImageData(x,y,1,1).data
+                let css_color = `rgb(${cls[0]},${cls[1]},${cls[2]})`
+                //console.log(css_color)
+                this.leds[index].setAttributeNS(null,"fill",css_color)
+                //console.log(this.leds[index])
+                index = index + 1
+            }
+        }
+    }
+
     extract(){
 
     }
+}
 
+class TileGroup{
+    constructor(svg){
+        this.svg = svg
+    }
+    async init(hostname){
+        config = await fetch_json("./config.json")
+        this.tiles = []
+        config.tiles[hostname].forEach((tile_cfg)=>{
+            let tile = new Tile(this.svg)
+            tile.init(tile_cfg.x,tile_cfg.y)
+            this.tiles.push(tile)
+        })
+    }
+    update_canvas(context){
+        this.tiles.forEach((tile)=>{
+            tile.update_canvas(context)
+        })
+    }
 }
 
 export{
-    Tile
+    TileGroup
 }
